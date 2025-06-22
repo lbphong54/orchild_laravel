9f2104d011eb01ef3eb003965e88f12d3a27325c
nhattan <nhattanidilian@gmail.com>
2025-06-22 23:08:28 +0700

    commit ok

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ use App\Http\Controllers\Api\AuthController;
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/restaurants/{id}/related', [RestaurantController::class, 'related']);
 
 // Restaurant routes
 Route::get('/restaurants', [RestaurantController::class, 'index']);
